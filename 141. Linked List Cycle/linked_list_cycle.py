33d6c6864ce728ed0c541a3d2f305cb730626983
SAJJAD FAZELI <fazeli.s.sajjad@gmail.com>
2025-07-04 13:34:32 -0700

    Update linked_list_cycle.py

--- 141. Linked List Cycle/linked_list_cycle.py
+++ 141. Linked List Cycle/linked_list_cycle.py
@@ -1,1 +1,24 @@
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
+
+
+# Using Hashmap
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
