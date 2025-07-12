c287db8c3029372dc9e8ea02ec8ed32a349b5bcc
SAJJAD FAZELI <fazeli.s.sajjad@gmail.com>
2025-07-11 18:57:43 -0700

    Update make_steing_great.py

--- Make String Great/make_steing_great.py
+++ Make String Great/make_steing_great.py
@@ -2,7 +2,7 @@ class Solution:
   def make_string_great(s):
     stack = []
     for char in s:
-      if abs(ord(stack[-1]) - ord(char)) == 32:
+      if stack  and abs(ord(stack[-1]) - ord(char)) == 32:
         stack.pop()
       else:
         stack.append(char)
