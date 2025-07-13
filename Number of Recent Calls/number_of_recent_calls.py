8337e8b07fbfc948b2fbe317d7aaf1d2d839dedc
SAJJAD FAZELI <fazeli.s.sajjad@gmail.com>
2025-07-13 10:31:28 -0700

    Create number_of_recent_calls.py

---
+++ Number of Recent Calls/number_of_recent_calls.py
@@ -0,0 +1,11 @@
+from collection import deque
+
+class RecentCounter:
+  def __init__(self):
+    self.requests = deque()
+  def ping(self, t):
+    self.requests.append(t):
+    while self.requests[0] < t - 3000:
+      self.requests.popleft()
+    return len(self.requests)
+    
