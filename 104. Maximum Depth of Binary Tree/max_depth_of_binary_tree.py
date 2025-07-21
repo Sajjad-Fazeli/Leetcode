7959958d8c62071467b7ba9419d6a257274393ca
SAJJAD FAZELI <fazeli.s.sajjad@gmail.com>
2025-07-21 08:07:35 -0700

    add iterative approach

--- 104. Maximum Depth of Binary Tree/max_depth_of_binary_tree.py
+++ 104. Maximum Depth of Binary Tree/max_depth_of_binary_tree.py
@@ -5,5 +5,18 @@ class Solution:
       left_depth = self.maxDepth(root.left)
       right_depth = self.maxDepth(root.right)
       return max(left, right) + 1
-
+    def maxDepth_iter(root):
+        if not root:
+            return 0
+        ans = 0
+        stack = [(root,1)]
+        while stack:
+            node, depth = stack.pop()
+            ans = max(ans, depth)
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return ans
+            
       
